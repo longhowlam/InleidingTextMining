8c81eff32f41df2b7cbc82758630d32b3638d419
longhowlam <longhowlam@gmail.com>
2017-09-26 19:55:35 +0100

    added data set

---
+++ glove.R
@@ -0,0 +1,71 @@
+library(feather)
+library(text2vec)
+library(dplyr)
+library(anytime)
+library(ggplot2)
+library(stringr)
+
+####  import scraped news articles from RTL
+RTLN = readRDS("newsarticles.RDs")
+
+### overview of articles per day
+hist(RTLN$date, breaks = "month")
+
+### text mine, tokenize etc.
+RTLNEWS_tokens = RTLN$value %>%
+  word_tokenizer
+
+##### and use the tokens to create an iterator and vocabular
+it_train = itoken(
+  RTLNEWS_tokens, 
+  ids = RTLN$nid,
+  progressbar = TRUE
+)
+
+
+stopwoorden = readRDS("stopwoorden.RDs")
+
+vocab = create_vocabulary(
+  it_train, 
+  ngram = c(ngram_min = 1L, ngram_max = 1L),
+  stopwords = stopwoorden
+)
+
+pruned_vocab = prune_vocabulary(
+  vocab, 
+  term_count_min = 25 ,
+  doc_proportion_max = 0.85
+)
+
+print("*** vocab generated****")
+print(pruned_vocab)
+
+vectorizer <- vocab_vectorizer(
+  pruned_vocab, 
+  # don't vectorize input
+  grow_dtm = FALSE, 
+  # use window of 5 for context words
+  skip_grams_window = 5L
+)
+
+tcm <- create_tcm(it_train, vectorizer)
+dim(tcm)
+
+#######  Glove word embeddings
+
+glove = GlobalVectors$new(word_vectors_size = 250, vocabulary = pruned_vocab, x_max = 10)
+glove$fit(tcm, n_iter = 30)
+word_vectors = glove$get_word_vectors()
+
+dim(word_vectors)
+word_vectors[1,]
+
+
+###### distances between words....
+WV <- word_vectors["parijs", , drop = FALSE] 
+cos_sim = sim2(x = word_vectors, y = WV, method = "cosine", norm = "l2")
+head(sort(cos_sim[,1], decreasing = TRUE), 20)
+
+
+
+
